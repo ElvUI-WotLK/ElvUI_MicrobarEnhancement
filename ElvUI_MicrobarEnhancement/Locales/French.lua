aec0d1260774ca06c010d1cdee9df4ee66b86915
Apollyonn <jellefenxd@gmail.com>
2018-06-18 16:57:04 +0300

    update

---
+++ ElvUI_MicrobarEnhancement/Locales/French.lua
@@ -0,0 +1,21 @@
+-- French localization file for frFR.
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ElvUI", "frFR")
+if not L then return; end
+
+-- Config
+L["As Letters"] = true
+L["Transparent Backdrop"] = true
+L["Replace icons with just letters"] = true
+
+-- Symbols
+L["ACHIEVEMENT_SYMBOL"] = "A"
+L["CHARACTER_SYMBOL"] = "C"
+L["HELP_SYMBOL"] = "?"
+L["LFD_SYMBOL"] = "D"
+L["MENU_SYMBOL"] = "M"
+L["PVP_SYMBOL"] = "P"
+L["QUEST_SYMBOL"] = "Q"
+L["TALENTS_SYMBOL"] = "T"
+L["SOCIAL_SYMBOL"] = "F"
+L["SPELLBOOK_SYMBOL"] = "S"
---
+++ ElvUI_MicrobarEnhancement/Locales/Russian.lua
@@ -0,0 +1,21 @@
+﻿--Файл локализации для ruRU
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ElvUI", "ruRU")
+if not L then return end
+
+-- Config
+L["As Letters"] = "В виде букв"
+L["Transparent Backdrop"] = "Прозрачный фон"
+L["Replace icons with just letters"] = "Заменить иконки буквами"
+
+-- Symbols
+L["ACHIEVEMENT_SYMBOL"] = "A"
+L["CHARACTER_SYMBOL"] = "C"
+L["HELP_SYMBOL"] = "?"
+L["LFD_SYMBOL"] = "D"
+L["MENU_SYMBOL"] = "M"
+L["PVP_SYMBOL"] = "P"
+L["QUEST_SYMBOL"] = "Q"
+L["TALENTS_SYMBOL"] = "T"
+L["SOCIAL_SYMBOL"] = "F"
+L["SPELLBOOK_SYMBOL"] = "S"
